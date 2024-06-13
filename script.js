60a76cd4d6ab9df70bc6884839ff332405834324
PriyanshuPandey10 <pandey.priyanshu10@gmail.com>
2024-06-13 11:10:36 +0530

    Dynamic Routing

--- script.js
+++ script.js
@@ -16,8 +16,20 @@ app.get('/', function (req, res) {
 app.get('/profile', function (req, res) {
     res.send('Hello World from Profile');
   })
+
+  //Dynamic Route
+app.get('/profile/:user', function (req, res) {
+    res.send(`Hello From ${req.params.user}`);
+  })
+
 app.get('/contact', function (req, res) {
     res.send('Hello World from Contact');
+  })
+
+    //Dynamic Route
+    
+app.get('/contact/:user', function (req, res) {
+    res.send(`Contact to ${req.params.user}`);
   })
   app.get('/home', function (req, res) {
     res.send('Hello World from Home');
